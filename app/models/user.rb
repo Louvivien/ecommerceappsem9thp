6f63cfd205de68d69e9e73ad987efa5811a9ca27
Alexisreau97 <alexis.reaupro@gmail.com>
2018-11-26 15:29:54 +0100

    add jointures

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   	belongs_to :cart
-  	has_many	:orders
+  	has_many   :orders
+end
