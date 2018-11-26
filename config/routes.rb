ce54fb6d1c9423b21abb3c7bd01e21e2b3d6a28a
Louvivien <vivien.richaud@gmail.com>
2018-11-26 14:33:33 +0100

    resolved conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
 
   get 'home/index'
 
 	root 'home#index'
-
-=======
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> parent of f0d9606... ajout systeme login logout signup
-end
