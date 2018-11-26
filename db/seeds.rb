ef88e0e4ce46f6cce4031403d074710f73d085fd
ratsimisampy <ratsimisampy@gmail.com>
2018-11-26 15:38:18 +0100

    mes Devise et View

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |item|
+    myItems = Item.create!(title: Faker::Artist.name, description: "description du produit", 
+    price: Faker::Commerce.price, image_url: Faker::Avatar.image("my-own-slug") )
+
+end
